40969045474ea16a3706b25409ffef2fa7654892
Kod-dasm <54396012+Kod-dasm@users.noreply.github.com>
2021-12-15 18:59:05 +0500

    created left and right containers for lists

---
+++ src/App/App.js
@@ -0,0 +1,22 @@
+
+export default {
+  data() {
+    return {
+      lists: [
+        { name: 'List 1', isOpen: false, isVisible: false, items: [
+          { name: 'item 1', count: 3, color: 'black', isVisible: false },
+          { name: 'item 2', count: 6, color: 'blue', isVisible: false },
+          { name: 'item 3', count: 5, color: 'yellow', isVisible: false }
+        ]},
+        { name: 'List 2', isOpen: false, isVisible: false, items: [
+          { name: 'item 1', count: 3, color: 'black', isVisible: false },
+          { name: 'item 2', count: 6, color: 'blue', isVisible: false },
+          { name: 'item 3', count: 5, color: 'yellow', isVisible: false }
+        ]}
+      ]
+    }
+  },
+  metods: {
+
+  }
+}
